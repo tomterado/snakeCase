e4bd6d73ed992ef53f4169e4790498e816811559
Tom <tom.terado@gmail.com>
2019-02-19 11:56:43 +0900

    Finished

--- snakeCase.js
+++ snakeCase.js
@@ -1,5 +1,19 @@
-const snakeCase = function() {
+const snakeCase = function(string) {
 
+    string = string.replace(/\.\./g, " ");
+
+    if (string.indexOf(" ") < 0) {
+        string = string.replace(/([A-Z])/g," $1");
+    }
+
+
+    return string
+    .trim().
+    toLowerCase()
+    .replace(/[,\?\.]/g, "")
+    .replace(/\-/g, " ")
+    .split(" ")
+    .join("_");
 }
 
 module.exports = snakeCase
